f101098c6d8c03c649fe24eb5f20e177f7f7ec08
Yong <yongdev91@gmail.com>
2021-03-18 17:21:20 +0900

    App Config 설정(기획자)

---
+++ src/test/java/hello/core/RateDiscountTest.java
@@ -0,0 +1,39 @@
+package hello.core;
+
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RateDiscountTest {
+
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("vip는 10% 할인 적용")
+    void vip_o() {
+        // given
+        Member member = new Member(1L, "MemberA", Grade.VIP);
+
+        // when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("vip가 아니면 할인 되지 않는다")
+    void vip_x() {
+        // given
+        Member member = new Member(1L, "MemberA", Grade.BASIC);
+
+        // when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
